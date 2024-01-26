7100bf611ad6d52ae1a8107407012dc6e31521f7
Nuraiym <nuraim.ermekova@gmail.com>
2024-01-26 13:04:35 +0600

    Bakai 1
    Bakai 2
    
    Bakai 3
    
    Rebase

--- src/src/BanksKg/Bakai.java
+++ src/src/BanksKg/Bakai.java
@@ -1,4 +1,14 @@
 package BanksKg;
 
 public class Bakai {
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+
+
+        System.out.println("Hello 123");
+
+        System.out.println("HELLO 456");
+
+    }
+
 }
