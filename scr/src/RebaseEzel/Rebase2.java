a7734853e852bdb933ce76cfa6397c76503b1a06
Ezel <akezel1711@gmail.com>
2024-01-26 13:09:32 +0600

    Rebase General
    Rebase 2
    
    Rebase 3
    
    commit 4

---
+++ scr/src/RebaseEzel/Rebase2.java
@@ -0,0 +1,7 @@
+package RebaseEzel;
+
+public class Rebase2 {
+    public static void main(String[] args) {
+        System.out.println("Rebase2");
+    }
+}
---
+++ scr/src/RebaseEzel/Rebase3.java
@@ -0,0 +1,7 @@
+package RebaseEzel;
+
+public class Rebase3 {
+    public static void main(String[] args) {
+        System.out.println("Rebase 3");
+    }
+}
