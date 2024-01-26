8105334264b2fdeb726888866e1a5867862f8911
ruslan <ruskano1991@gmail.com>
2024-01-26 12:45:07 +0600

    ten

---
+++ src/src/ruslan/NewClass.java
@@ -0,0 +1,7 @@
+package ruslan;
+
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("Ten");
+    }
+}
--- src/src/ruslan/RuslanNewClass.java
+++ src/src/ruslan/RuslanNewClass.java
@@ -1,7 +1,5 @@
 package ruslan;
 
 public class RuslanNewClass {
-    public static void main(String[] args) {
-        System.out.println("Hi Fall");
-    }
+
 }
