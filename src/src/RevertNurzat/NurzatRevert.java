100a0ef2490dc23a1e832d6e7a772bb486f53f3a
Nurzat <bekeshevnurzat@gmail.com>
2024-01-26 12:44:25 +0600

    2 commit revert

---
+++ src/src/RevertNurzat/NurzatRevert.java
@@ -0,0 +1,8 @@
+package RevertNurzat;
+
+public class NurzatRevert {
+    public static void main(String[] args) {
+        System.out.println("Revert Test");
+        System.out.println("Revert Test 1");
+    }
+}
