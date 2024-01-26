e14b0d20bdd3938bb57ea7c8124d445e0b3cb2d4
Aizhan <aizhant51@gmail.com>
2024-01-26 10:20:17 +0300

    rebase 2

--- src/src/Rebase/aiznah.java
+++ src/src/Rebase/aiznah.java
@@ -4,4 +4,18 @@ public class aiznah {
     public static void main(String[] args) {
         System.out.println(" Aizhan");
     }
+    public String stringYak(String str) {
+        String front = "";
+        for (int i = 0; i< str.length();i++){
+            if (i+2 < str.length() && str.charAt(i)=='y' && str.charAt(i+2)=='k'){
+                i = i+2;
+            }
+            else {
+                front += str.charAt(i);
+            }
+        }
+        return front;
+
+    }
+
 }
