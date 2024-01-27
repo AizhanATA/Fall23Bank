379c03667655ffac29f64310c7eeca54909110d6
Asylbek Choturaliev <asikosan2014@gmail.com>
2024-01-27 17:09:47 +0600

    Revert "comm4 UPDATE"
    This reverts commit 409673b80aa09879d54ce5684e8b40631351f0ac.

--- src/src/Asyl/Rev2Asyl.java
+++ src/src/Asyl/Rev2Asyl.java
@@ -1,10 +1,4 @@
 package Asyl;
 
 public class Rev2Asyl {
-    public static void main(String[] args) {
-
-    }
-    public static void WhatYou(){
-        System.out.println("Say Hello");
-    }
 }
