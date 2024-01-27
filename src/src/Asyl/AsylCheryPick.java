d78e180d47ba23729a8c45d10126f65208bda088
Asylbek Choturaliev <asikosan2014@gmail.com>
2024-01-27 17:02:36 +0600

    C2

---
+++ src/src/Asyl/AsylCheryPick.java
@@ -0,0 +1,8 @@
+package Asyl;
+
+public class AsylCheryPick {
+    public static void main(String[] args) {
+        System.out.println("CherryPick");
+        System.out.println("add. -- commit -- git log --online -- switch branch");
+    }
+}
---
+++ src/src/Asyl/RevertAsyl.java
@@ -0,0 +1,5 @@
+package Asyl;
+
+public class RevertAsyl {
+    
+}
